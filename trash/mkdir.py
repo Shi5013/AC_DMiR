78551d30674264655da14d3a17e50420911c558f
Wenwei Shi <wwshi0428@outlook.com>
2024-01-06 09:37:05 +0800

    make dir for data2

---
+++ trash/mkdir.py
@@ -0,0 +1,15 @@
+import os
+
+base_folder = './data2/'
+
+for i in range(20):
+    patient_folder = os.path.join(base_folder, f'patient{i:02d}')
+    os.makedirs(patient_folder)
+
+    # 在每个patient文件夹下创建image和mask子文件夹
+    image_folder = os.path.join(patient_folder, 'image')
+    mask_folder = os.path.join(patient_folder, 'mask')
+    os.makedirs(image_folder)
+    os.makedirs(mask_folder)
+
+print("文件夹和子文件夹创建完成。")
